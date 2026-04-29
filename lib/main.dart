46b6c5ae4b18bd32c9538709bb527f39c949dcba
Bayu Seno Adjie <bayuadjie111@gmail.com>
2026-04-29 11:11:50 +0700

    menambahkan sqflite untuk uts

--- lib/main.dart
+++ lib/main.dart
@@ -1,15 +1,101 @@
 import 'package:flutter/material.dart';
+import 'package:sqflite/sqflite.dart';
+import 'package:path/path.dart' as p;
 
 void main() {
   runApp(const MainApp());
 }
 
-class MainApp extends StatelessWidget {
+class MainApp extends StatefulWidget {
   const MainApp({super.key});
 
   @override
+  State<MainApp> createState() => _MainAppState();
+}
+
+class _MainAppState extends State<MainApp> {
+  @override
   Widget build(BuildContext context) {
     return const MaterialApp(home: LitsUserDataPage());
+  }
+}
+
+class DatabaseHelper {
+  static Database? _database;
+
+  static Future<Database> get database async {
+    if (_database != null) return _database!;
+    _database = await initDB();
+    return _database!;
+  }
+
+  static Future<Database> initDB() async {
+    String path = p.join(await getDatabasesPath(), 'user_db.db');
+    return await openDatabase(
+      path,
+      version: 1,
+      onCreate: (db, version) {
+        db.execute(
+          "CREATE TABLE users (id INTEGER PRIMARY KEY AUTOINCREMENT,nama TEXT,umur INTEGER)",
+        );
+      },
+    );
+  }
+
+  static Future<Database> initDatabase() async {
+    final databasesPath = await getDatabasesPath();
+    final path = p.join(databasesPath, 'user_database.db');
+
+    return await openDatabase(
+      path,
+      version: 1,
+      onCreate: (db, version) {
+        db.execute('''
+          CREATE TABLE users (
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            nama TEXT NOT NULL,
+            umur INTEGER NOT NULL
+          )
+        ''');
+      },
+    );
+  }
+
+  //create
+  static Future<void> insertData(UserModel userModel) async {
+    final db = await database;
+    Map<String, dynamic> user = userModel.toJson();
+    await db.insert(
+      "users",
+      user,
+      conflictAlgorithm: ConflictAlgorithm.replace,
+    );
+  }
+
+  //read
+  static Future<List<UserModel>> getData() async {
+    final db = await database;
+    List<Map<String, Object?>> result = await db.query("users");
+
+    List<UserModel> users = result.map((usermap) {
+      return UserModel.formjson(usermap);
+    }).toList();
+
+    return users;
+  }
+
+  //update
+  static Future<int> updateData(int id, UserModel userModel) async {
+    final db = await database;
+    var user = userModel.toJson()..remove("id");
+
+    return await db.update("users", user, where: "id = ?", whereArgs: [id]);
+  }
+
+  //delete
+  static Future<int> deleteData(int id) async {
+    final db = await database;
+    return await db.delete("users", where: "id = ?", whereArgs: [id]);
   }
 }
 
@@ -26,18 +112,35 @@ class UserModel {
   int umur = 0;
 
   UserModel(this.id, {required this.nama, required this.umur});
+  // conver dari map ke model / hashmap ke model
+  factory UserModel.formjson(Map<String, dynamic> json) {
+    return UserModel(json["id"], nama: json["nama"], umur: json["umur"]);
+  }
+
+  //conver dari model ke map
+  Map<String, dynamic> toJson() {
+    return {"id": id, "nama": nama, "umur": umur};
+  }
 }
 
 class _LitsUserDataPageState extends State<LitsUserDataPage> {
   final TextEditingController nameCtrl = TextEditingController();
   final TextEditingController umurCtrl = TextEditingController();
 
-  List<UserModel> userList = [
-    UserModel(1, nama: "Satu", umur: 10),
-    UserModel(2, nama: "Dua", umur: 20),
-    UserModel(3, nama: "Tiga", umur: 30),
-    UserModel(4, nama: "Empat", umur: 40),
-  ];
+  List<UserModel> userList = [];
+  @override
+  void initState() {
+    super.initState();
+
+    _reloadData();
+  }
+
+  void _reloadData() async {
+    var users = await DatabaseHelper.getData();
+    setState(() {
+      userList = users;
+    });
+  }
 
   void form(int? id) {
     if (id != null) {
@@ -81,21 +184,17 @@ class _LitsUserDataPageState extends State<LitsUserDataPage> {
     );
   }
 
-  void save(int? id, String nama, int umur) {
+  void save(int? id, String nama, int umur) async {
+    var newUser = UserModel(null, nama: nama, umur: umur);
     if (id != null) {
-      var user = userList.firstWhere((data) => data.id == id);
-      setState(() {
-        user.nama = nama;
-        user.umur = umur;
-      });
+      await DatabaseHelper.updateData(
+        id,
+        UserModel(id, nama: nama, umur: umur),
+      );
     } else {
-      var nextId = userList.length + 1;
-      var newUser = UserModel(nextId, nama: nama, umur: umur);
-      setState(() {
-        userList.add(newUser);
-      });
+      await DatabaseHelper.insertData(newUser);
     }
-
+    _reloadData();
     Navigator.pop(context);
   }
 
@@ -113,8 +212,9 @@ class _LitsUserDataPageState extends State<LitsUserDataPage> {
             child: Text("Batal"),
           ),
           TextButton(
-            onPressed: () {
-              setState(() => userList.removeWhere((data) => data.id == id));
+            onPressed: () async {
+              await DatabaseHelper.deleteData(id);
+              _reloadData();
               Navigator.pop(context);
             },
             child: Text("Hapus"),
@@ -127,7 +227,7 @@ class _LitsUserDataPageState extends State<LitsUserDataPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text("User List")),
+      appBar: AppBar(title: Text("User Lits")),
       body: ListView.builder(
         itemCount: userList.length,
         itemBuilder: (cxt, i) => ListTile(
